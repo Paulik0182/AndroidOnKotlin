62b2087b944782936469915975f8ee68be12579f
Paveł <Paulik0182@gmail.com>
2021-12-29 17:18:05 +0300

    Implementing click buttons

--- app/src/main/java/com/android/androidonkotlin/MainActivity.java
+++ app/src/main/java/com/android/androidonkotlin/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.androidonkotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
         counterUpdate(counter);
 
+        minusButton.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                counterUpdate ( --counter );
+            }
+        } );
+
+        plusButton.setOnClickListener ( v ->
+                counterUpdate ( ++counter ) );
     }
 
     private  void counterUpdate (int counter){
