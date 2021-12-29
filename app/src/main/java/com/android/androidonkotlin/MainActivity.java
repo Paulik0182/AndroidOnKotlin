66830320547c625826c38befc5fc07813adecd58
Paveł <Paulik0182@gmail.com>
2021-12-29 17:08:55 +0300

    Initialization -> Button & TextView

--- app/src/main/java/com/android/androidonkotlin/MainActivity.java
+++ app/src/main/java/com/android/androidonkotlin/MainActivity.java
@@ -3,12 +3,26 @@ package com.android.androidonkotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button minusButton = null;
+    private Button plusButton = null;
+    private TextView textViewCounter = null;
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        minusButton = findViewById ( R.id.button_minus );
+        plusButton = findViewById ( R.id.button_plus );
+        textViewCounter = findViewById ( R.id.text_view_counter );
+
     }
+
 }
