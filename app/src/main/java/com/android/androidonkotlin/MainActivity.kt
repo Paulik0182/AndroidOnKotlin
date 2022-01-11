3003b71285595d393720a2ae63e3bfc03135ce08
Paveł <Paulik0182@gmail.com>
2022-01-11 20:03:36 +0300

    Configure Kotlin and conversion kotlin -> class MainActivity

--- app/src/main/java/com/android/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/android/androidonkotlin/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.android.androidonkotlin;
+package com.android.androidonkotlin
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.androidonkotlin.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
