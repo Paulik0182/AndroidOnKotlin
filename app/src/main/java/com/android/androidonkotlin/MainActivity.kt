d7665b6f9fe068e956b52de07c8dc8d4ca5a9d3a
Paveł <Paulik0182@gmail.com>
2022-01-11 20:15:51 +0300

    Binding -> class MainActivity

--- app/src/main/java/com/android/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/android/androidonkotlin/MainActivity.kt
@@ -3,10 +3,16 @@ package com.android.androidonkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.androidonkotlin.R
+import com.android.androidonkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
